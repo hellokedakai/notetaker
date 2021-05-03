d2bef8836a9f988f151ca19d1dc1b2de30f4b69f
Kedakai <nicoleteeth@ucla.edu>
2021-05-03 13:11:23 -0700

    add res.json for test

--- server.js
+++ server.js
@@ -13,8 +13,46 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get('/api/notes', (req, res) => {
-    console.log("hello!");
+    // res.send("hello!");
+    res.json(notes);
+
 });
+
+// this function will take in req.query as an argument and filter through the notes accordingly, returning the new filtered aray.
+// function filterByQuery(query, notesArray) {
+//     let filteredResults = notesArray;
+//     if(query.title) {
+//         filteredResults = filteredResults.filter(note => note.title === query.title);
+//     }
+//     return filteredResults;
+// }
+// database json structure
+/* [
+    {
+        "title":"Test Title",
+        "text":"Test text"
+    }
+]
+*/
+
+//save icon works (save -> note to array)
+//assign IDs
+//related function: saveNote, handleNoteSave
+
+//saved note appears on the left
+//related function: renderNoteList
+
+//saved note appears on the right
+//related function: renderActiveNote, handleNoteView
+
+
+//delete note works (remove note from array)
+//related function: deleteNote, handleNoteDelete
+
+//new note writing works (pencil icon => new note to an array)
+
+
+//functions from index.js
 
 // const apiRoutes = require("./routes/apiRoutes");
 // const htmlRoutes = require("./routes/htmlRoutes");
@@ -30,17 +68,6 @@ app.get('/notes', (req,res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
-
-//save icon works
-
-//saved note appears on the left
-
-//saved not appears on the right
-
-//new note writing
-
-//functions from index.js
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
